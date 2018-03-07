28451ca3be384772e01df1e452e5567571f531fa
Kanishka <kanishkamakhija007@gmail.com>
2018-03-07 12:29:11 +0530

    ADDED: Bootstrap modal and play agin functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
-/*
- * Create a list that holds all of your cards
- */
+
+ /*global $, jQuery*/
+ /*
+  * Create a list that holds all of your cards
+  */
 var list_cards = ["fa fa-diamond","fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube" ];
 
-var card1, card2;
+// Container to fetch start ratings
+var container = $(".fa.fa-star");
+
+// List to match the open cards and save the finally matched cards
 var open_list = [];
 var match_list = [];
-window.onload = function() {
+let click=false;
+var countDown;
+
+// Fetch the modal to display the status of end game
+var modals = document.getElementById('win-modal');
+// Button that allows to play again
+var playagnBtn = document.getElementsByClassName('close')[0];
+//Count the number of moves of aplayer
+var open_modal = document.getElementById("open_modal");
+
+//Function to shuffle the cards
+function shuffleit() {
     list_cards = shuffle(list_cards);
     shuffle_cards(list_cards);
 };
 
+// Shuffle cards on loading the game
+window.onload = shuffleit();
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -40,26 +58,37 @@ function shuffle_cards(array)
         {
             var curr = list[i];
             curr = curr.getElementsByTagName("i")[0].setAttribute("class", array[i]);
-            console.log(curr);
         }
 }
 
+
+//Tracks the performance by changinf the start ratings
+function moves1(){
+  if (document.getElementById("numberMoves").innerText ==16){
+      $(container[2]).removeClass("fa fa-star") ;
+  }
+  else if (document.getElementById("numberMoves").innerText ==32) {
+    $(container[1]).removeClass("fa fa-star");
+  }
+
+
+}
+
+//Function that checks whether the two open cards are matching or not
 function ismatch()
 {
     let class1 = $(open_list[0]).children('i').attr('class');
     let class2 = $(open_list[1]).children('i').attr('class');
-    console.log("class1:",class1);
-    console.log("class2:",class2);
+
     if(class1 === class2)
     {
-        console.log("match card");
+
         $(open_list[0]).addClass("match");
         $(open_list[1]).addClass("match");
         match_list.push(open_list[0]);
         match_list.push(open_list[1]);
     }
     else {
-        console.log("mismatch card");
         $(open_list[0]).removeClass("open show");
         $(open_list[1]).removeClass("open show");
     }
@@ -67,23 +96,122 @@ function ismatch()
     return;
 }
 
+
+//Function that displays the content of number of moves, start rating and timings in which user won the game
+function win(){
+  if (match_list.length === 16){
+    clearInterval(timer);
+    let newP = document.createElement('p');
+    modals.style.display = 'block';
+    newP.textContent = "You Win in " + document.getElementById("numberMoves").innerText + " moves. With a star rating of " + $(".fa.fa-star").length + " in " + document.getElementById("minutes").innerText + " minutes and "+  document.getElementById("seconds").innerText + " seconds. Do you want to play again...? Click OK to continue.";
+    open_modal.appendChild(newP);
+    modal();
+  }
+}
+
+function modal() {
+  window.addEventListener('click', outsideClick);
+}
+
+// Function to close modal
+function closeModal() {
+  click = false;
+  modals.style.display = 'none';
+  $("ul.deck li").removeClass("open show match");
+  list_cards = shuffle(list_cards);
+  shuffle_cards(list_cards);
+  reset();
+}
+
+// Function to close modal if outside click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modals.style.display = 'none';
+    $("ul.deck li").removeClass("open show match");
+    list_cards = shuffle(list_cards);
+    shuffle_cards(list_cards);
+  }
+  reset();
+}
+
+
+//Function to flip the cars by removing class open and show
+function flipping() {
+  if ( $(".open").length===1){
+     $('.card').removeClass('open').removeClass('show');
+     open_list = [];
+     setTimeout(remove,701)
+document.getElementById("numberMoves").innerText--;
+  }
+}
+
+//Function that starts the timer when the first card is clicked
+  function timer(){
+      var sec = 0;
+      function pad ( val ) { return val > 9 ? val : "0" + val; }
+          countDown = setInterval( function(){
+          document.getElementById("seconds").innerHTML=pad(++sec%60);
+          document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+          if (match_list.length === 16){
+                 clearInterval(countDown);
+           }
+         }, 1000);
+  }
+
+
+//Funtion to empty the match list
+function remove (){
+    match_list.pop();
+    match_list.pop();
+  }
+
+
+//Function to reset the game to its initiall stage
+function reset (){
+    $(".card").removeClass("match").removeClass("open").removeClass("show");
+    document.getElementById("numberMoves").innerText = 0;
+    $(container).addClass("fa fa-star");
+    list_cards = shuffle(list_cards);
+    shuffle_cards(list_cards);
+    match_list = [];
+    document.getElementById("minutes").innerHTML='00';
+    document.getElementById("seconds").innerHTML='00';
+    click = false;
+    clearInterval(countDown);
+}
+
 $(document).ready(function() {
-    $("ul.deck li").click(function() {
-        open_list.push(this);
-        $(this).addClass("show open");
-        console.log(open_list);
-        if(open_list.length === 2)
-        {
-            setTimeout(ismatch, 3000);
-        }
-        console.log(match_list);
+
+    $("div.restart i").click(function() {
+    click = false;
+    reset();
+    $(container).addClass("fa fa-star");
     });
 
-    //lid.classsList.toggle('flip');
+    $("#win-modal").click(function() {
+      window.location.reload();
 
+    });
+
+    $("ul.deck li").click(function() {
+      console.log(click);
+    if (!click)
+    {
+    timer();
+    }
+    moves1();
+    click=true;
+    open_list.push(this);
+    $(this).addClass("show open");
+    if(open_list.length === 2)
+    {
+      document.getElementById("numberMoves").innerText++;
+      setTimeout(flipping, 402);
+      setTimeout(ismatch,700);
+      setTimeout(win,1000);
+    }
+    });
 });
-
-
 
 
 
